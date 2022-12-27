0fe0f559be61cbdc1d10fa01f51903583379777b
LIUAofStevensTransport <58896250+LIUAofStevensTransport@users.noreply.github.com>
2022-12-27 19:18:48 +0300

    Some improvements
    Logo, menus, title are dynamic. Got really stuck with how to make episodes dynamic.

--- functions.php
+++ functions.php
@@ -1,5 +1,24 @@
 <?php 
 
+function innowisetest_theme_support() {
+
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+};
+
+add_action('after_setup_theme', 'innowisetest_theme_support');
+
+function innowisetest_menus() {
+
+    $menuLocations = array(
+        'primary' => "Top Right Menu",
+        'secondary' => "Footer Extended Menu"
+    );
+    register_nav_menus($menuLocations);
+}
+
+add_action('init', 'innowisetest_menus');
+
 function innowisetest_register_styles() {
 
     wp_enqueue_style('innowisetest-styles', get_template_directory_uri() . "/style.css", array('innowisetest-gstatic', 'innowisetest-googleapis', 'innowisetest-font' ), '1.0', 'all');
--- header.php
+++ header.php
@@ -4,19 +4,12 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   <!-- <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap"
-      rel="stylesheet"
-    />
-     <link rel="stylesheet" href="styles.css" /> -->
+
     <?php
-     wp_head() 
+     wp_head();
     ?>
 
 
-    <!-- <title>Castaway Podcasts /Innowise PHP test mockup/</title> -->
   </head>
   <body>
     <div class="narrow-container">
@@ -25,29 +18,35 @@
       <header class="header flex flex-row flex-nowrap justify-around">
         <div class="header__container-logo container-logo">
           <a href="index.html"
-            ><img src="<?php echo get_template_directory_uri(); ?>/assets/images/Castaway_logo.svg"  alt="Podcast Logo"
+            >
+            <?php
+              if (function_exists('the_custom_logo')) {
+                $custom_logo_id = get_theme_mod('custom_logo');
+                $logo = wp_get_attachment_image_src($custom_logo_id);
+              }
+
+              
+            ?>
+            <img src="<?php echo $logo[0]; ?>"  alt="Podcast Logo"
           /></a>
         </div>
         <div class="header__container-nav container-nav flex justify-end">
           <nav class="container-nav__nav nav">
-            <ul class="nav__link-list link-list flex">
-              <li class="link-list__nav-item nav-item">
-                <a class="nav-item__nav-link nav-link__home" href="#">Home</a>
-              </li>
-              <li class="link-list__nav-item nav-item">
-                <a class="nav-item__nav-link nav-link__episodes" href="#"
-                  >Episodes</a
-                >
-              </li>
-              <li class="link-list__nav-item nav-item">
-                <a class="nav-item__nav-link nav-link__about" href="#">About</a>
-              </li>
-              <li class="link-list__nav-item nav-item">
-                <a class="nav-item__nav-link nav-link__contact" href="#"
-                  >Contact</a
-                >
-              </li>
-            </ul>
+            <?php
+              wp_nav_menu(
+                array(
+                  'menu' => 'primary',
+                  'container' => '',
+                  'theme_location' => 'primary',
+                  'items_wrap' => '<ul id="" class="nav__link-list link-list flex">%3$s</ul>'
+
+                )
+              
+              )
+            ?>
+
+
+            
           </nav>
         </div>
       </header>
